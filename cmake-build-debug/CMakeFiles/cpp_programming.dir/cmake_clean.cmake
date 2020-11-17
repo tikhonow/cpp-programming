e88b97b54fdefe06fc19825e11672774eb55cca5
tikhonow <tihoo99@icloud.com>
2020-11-17 12:31:07 +1000

    first tournament is done

---
+++ cmake-build-debug/CMakeFiles/cpp_programming.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_programming.dir/2Cats/GameDatabase/main.cpp.o"
+  "cpp_programming"
+  "cpp_programming.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_programming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
